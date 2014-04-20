a5d4e16365cdfc7c065b0e6c13525c60503faec9
Cristian Monterroza <cmonterroza@wrkstrm.me>
2014-04-19 18:06:45 -0700

    Linting w/ Rubocop.

--- io_practice.rb
+++ io_practice.rb
@@ -3,42 +3,41 @@ def number_guess
   found = false
 
   guesses = 0
-  while !found do
-    puts "Pick a number between 1 - 100: "
+  until found
+    puts 'Pick a number between 1 - 100: '
     guess = gets.chomp.to_i
     if guess == number
       found = true
     elsif guess < number
-      puts "Too low! Try again"
+      puts 'Too low! Try again'
     else
-      puts "Too High! Try again"
+      puts 'Too High! Try again'
     end
 
     guesses += 1
   end
 
-  puts "You win! It took this many tries #{guesses}"
+  puts 'You win! It took this many tries #{guesses}'
 end
 
 # number_guess
 
 def shuffle_lines
-  puts "Enter a file name: "
+  puts 'Enter a file name: '
   file_name = gets.chomp
 
   lines = []
 
   File.foreach(file_name) do |line|
-    lines << line.chomp#.gets.chomp
+    lines << line.chomp
   end
 
   lines.shuffle!
 
-  new_f = File.open("#{file_name}-shuffled.txt",'w')
+  new_f = File.open("#{file_name}-shuffled.txt", 'w')
   lines.each do |line|
     new_f.puts line
   end
-
 end
 
 shuffle_lines
